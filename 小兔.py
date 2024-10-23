81708aa4acfb2ae577cb72c42f6585f130c3ccea
320284608 <75968479+320284608@users.noreply.github.com>
2024-10-23 09:43:59 +0800

    Create 小兔.py

---
+++ 小兔.py
@@ -0,0 +1,22 @@
+import requests
+import time
+
+# 获取当前的10位时间戳
+current_timestamp = int(time.time())
+
+url = "https://mapi.xiaotucc.com/we/user/lottery/take"
+headers = {
+    "Host": "mapi.xiaotucc.com",
+    "Connection": "keep-alive",
+    "charset": "utf-8",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 14; MI 9 Build/UKQ1.231003.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.188 Mobile Safari/537.36 XWEB/1260093 MMWEBSDK/20240501 MMWEBID/7437 MicroMessenger/8.0.50.2701(0x2800325A) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android",
+    "content-type": "application/x-www-form-urlencoded",
+    "Accept-Encoding": "gzip,compress,br,deflate",
+    "Referer": "http"
+}
+
+# 替换timestamp为当前时间戳
+data = f"uid=3673043&timestamp={current_timestamp}&token=8B9C6C0FBBFD98386482C5F9C1948103971EC622"
+
+res = requests.post(url, headers=headers, data=data)
+print(res.text)
